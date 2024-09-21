23b13d44a634fe5dc531f0c5996a2a12657726d5
tba117 <tbiayt1107@icloud.com>
2024-09-21 22:52:02 +0900

    'added'

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c4m5ru74e=0=aeevzty$%@7nqg#(1s75iv_#cpkr5+5upppta='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
