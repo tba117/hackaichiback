ce8d7f521dc4abd024dd0c9d459232aa422d015a
tba117 <tbiayt1107@icloud.com>
2024-09-23 13:32:16 +0900

    added

--- project/settings.py
+++ project/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'images'
