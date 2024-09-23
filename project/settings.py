7c4aee0db705fbd8ca8268939b55f698a7dd1040
tba117 <tbiayt1107@icloud.com>
2024-09-23 13:40:15 +0900

    added

--- project/settings.py
+++ project/settings.py
@@ -140,6 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # フロントエンドのオリジン
+    'https://aichihack-back-153bffff1dd9.herokuapp.com',  # バックエンドのURL（オプション）
+]
+
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'images'
