b5b0746c6f225d9af4970d472d2f1b4d95fa59ca
tba117 <tbiayt1107@icloud.com>
2024-09-23 13:51:14 +0900

    added

--- project/settings.py
+++ project/settings.py
@@ -140,6 +140,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # フロントエンドのオリジン
     'https://aichihack-back-153bffff1dd9.herokuapp.com',  # バックエンドのURL（オプション）
