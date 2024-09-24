6bad3286a1b1aa88856635f62843426f2c5ebe1f
tba117 <tbiayt1107@icloud.com>
2024-09-24 10:59:09 +0900

    'added'

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
 
     path('generate-questions/<str:user_id>/', other_views.generate_questions, name='generate_questions'),
     path('generate-profile/<str:user_id>/', other_views.generate_profile, name='generate_profile'),
-    path('get_task_status/<str:task_id>/', other_views.get_task_status, name='generate_profile'),
 
     path('matching/', matching_views.match_user, name='matching'),
     path('get_matched_users/', matching_views.get_matched_users, name='matching-list'),
