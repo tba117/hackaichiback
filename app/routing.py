b799eb674f69c7435d863a059a420d69bfac929f
tba117 <tbiayt1107@icloud.com>
2025-01-25 21:29:35 +0900

    add Procfile

--- app/routing.py
+++ app/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
+    path(r'ws/chat/(?P<room_name>\w+)', ChatConsumer.as_asgi()),
 ]
