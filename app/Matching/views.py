109553427159acf85a59b30aa12f6e39f19546b5
tba117 <tbiayt1107@icloud.com>
2025-01-27 19:37:08 +0900

    'add'

--- app/Matching/views.py
+++ app/Matching/views.py
@@ -116,14 +116,14 @@ def get_matched_users(request):
 
         matched_user_info = [
             {
-            "user_id": matched_users.user_id,
-            "username": matched_users.username,
-            "self_introduction": matched_users.self_introduction,
-            "department": matched_users.department,
-            "skils": matched_users.skils,
-            "hobbys": matched_users.hobbys,
-            "user_manual": matched_users.user_manual,
-            "snsid": matched_users.snsid,
+            "user_id": user.user_id,
+            "username": user.username,
+            "self_introduction": user.self_introduction,
+            "department": user.department,
+            "skils": user.skils,
+            "hobbys": user.hobbys,
+            "user_manual": user.user_manual,
+            "snsid": user.snsid,
             }
             for user in matched_users
         ]
