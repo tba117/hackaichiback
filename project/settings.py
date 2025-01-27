34c2bd3953fa6bfcda55b2dc0689ee87eb241dbf
tba117 <tbiayt1107@icloud.com>
2025-01-27 18:36:20 +0900

    'add'

--- project/settings.py
+++ project/settings.py
@@ -198,7 +198,6 @@ CHANNEL_LAYERS = {
             'hosts': [{
                 'address': ('ec2-34-236-76-249.compute-1.amazonaws.com', 19190),
                 'password': 'p7e46e50767d81cebb364174e1d4f9a14f4e222fde3a4cb2a7886d4b896b638a0',
-                'ssl': True,  # SSLを有効にする
             }],
         },
     },
