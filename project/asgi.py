49bf56046ca2988b76f5bf29fb7af6128c15aff2
tba117 <tbiayt1107@icloud.com>
2025-01-27 15:19:05 +0900

    'add'

--- project/asgi.py
+++ project/asgi.py
@@ -9,7 +9,7 @@ import app.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 #application = get_asgi_application()
-django_asgi_app = get_asgi_application()
+django_asgi_app = get_asgi_application()  # 通常のHTTPリクエストを処理する
 
 application = ProtocolTypeRouter( {
     'http': get_asgi_application(),
