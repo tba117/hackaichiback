356a0f65c00d7a8add751d0979141cbfb0c2135f
tba117 <tbiayt1107@icloud.com>
2025-01-27 20:41:41 +0900

    'add'

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import User, Chat, ChatRoom
 
-admin.site.register(User, ChatRoom, Chat)
+admin.site.register(User)
+admin.site.register(Chat)
+admin.site.register(ChatRoom)
